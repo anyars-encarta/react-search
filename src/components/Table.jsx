84711402a88cfc6920fa0d637a2f2a094e518050
anyars-encarta <anyarsencarta@gmail.com>
2024-08-17 12:26:59 +0000

    Implement table search

---
+++ src/components/Table.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Table = ({ data }) => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                </tr>
+
+                {data.map(({ id, first_name, last_name, email, gender }) => (
+                    <tr key={id}>
+                        <td>{first_name}</td>
+                        <td>{last_name}</td>
+                        <td>{email}</td>
+                        <td>{gender}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
